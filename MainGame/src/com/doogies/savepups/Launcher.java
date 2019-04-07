3681bf6ba54af158b90a08503778713834fc2d3a
Jonathan Alphonso <31195359+Rabadunk@users.noreply.github.com>
2019-04-07 21:07:02 +1200

    following tutorial by CodeNMore

---
+++ MainGame/src/com/doogies/savepups/Launcher.java
@@ -0,0 +1,12 @@
+package com.doogies.savepups;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        Game game = new Game("Save The Puppies", 640, 300);
+        game.start();
+
+    }
+
+}
