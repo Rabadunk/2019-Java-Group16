26f90f0814435ade8fc994046cf9d8e8ca1202dd
rodgergit <rodgerthatxd@gmail.com>
2019-04-18 19:46:01 +1200

    merge error fix

--- MainGame/src/com/doogies/savepups/entities/creatures/Creature.java
+++ MainGame/src/com/doogies/savepups/entities/creatures/Creature.java
@@ -6,8 +6,6 @@ import com.doogies.savepups.tiles.Tile;
 
 public abstract class Creature extends Entity {
 
-    public static final int DEFAULT_HEALTH = 10;
-
     public static final float DEFAULT_SPEED = 3.0f;
     public static final int DEFAULT_CREATURE_WIDTH = 64,
                             DEFAULT_CREATURE_HEIGHT = 64;
